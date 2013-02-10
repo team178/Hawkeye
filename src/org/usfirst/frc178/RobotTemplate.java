a00865fb79f0cbe5a79c2dd2107228c5dc5383c2
Brandon Bald <Webmaster@FarmingtonRobotics.org>
2013-02-10 18:32:48 -0500

    Tabs instead of spaces

--- src/org/usfirst/frc178/RobotTemplate.java
+++ src/org/usfirst/frc178/RobotTemplate.java
@@ -9,55 +9,51 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 
 import org.usfirst.frc178.devices.Motors;
 
-public class RobotTemplate extends IterativeRobot 
-{
-    DriverStationLCD dsout;
-    private DriverStation driverStation;
-    //private DashboardHigh dashboardHigh;
+public class RobotTemplate extends IterativeRobot  {
+	DriverStationLCD dsout;
+	private DriverStation driverStation;
+	//private DashboardHigh dashboardHigh;
 
-    private Drivetrain drivetrain;
-    private Shooter shooter;
-        
-    private Sensors sensors;
-    private Motors motors;
-    private HumanControl humanControl;
+	private Drivetrain drivetrain;
+	private Shooter shooter;
 
-    private Watchdog watchdog;
+	private Sensors sensors;
+	private Motors motors;
+	private HumanControl humanControl;
 
-    public void robotInit() 
-    {
-        driverStation = DriverStation.getInstance();
-	//dashboardHigh = new DashboardHigh();
+	private Watchdog watchdog;
 
-        motors = new Motors();
-        sensors = new Sensors();
-        humanControl = new HumanControl();
-                
-	drivetrain = new Drivetrain(motors,humanControl);
-        shooter = new Shooter(motors,sensors,humanControl);
+	public void robotInit() {
+		driverStation = DriverStation.getInstance();
+		//dashboardHigh = new DashboardHigh();
 
-        dsout = DriverStationLCD.getInstance();
-	dsout.updateLCD();
+		motors = new Motors();
+		sensors = new Sensors();
+		humanControl = new HumanControl();
 
-	watchdog = Watchdog.getInstance();
-    }
+		drivetrain = new Drivetrain(motors,humanControl);
+		shooter = new Shooter(motors,sensors,humanControl);
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() 
-    {
+		dsout = DriverStationLCD.getInstance();
+		dsout.updateLCD();
 
-    }
+		watchdog = Watchdog.getInstance();
+	}
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() 
-    {
-        drivetrain.drive();
-        shooter.shoot();
-        watchdog.feed();
-    }
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic() {
+
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+		drivetrain.drive();
+		shooter.shoot();
+		watchdog.feed();
+	}
 
 }
