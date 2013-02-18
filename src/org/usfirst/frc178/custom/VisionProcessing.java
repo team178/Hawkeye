a2b9c8a7614ee750828b1f53c3a3ecb5d3358aae
Brandon Cheng <bcheng.gt@gmail.com>
2013-02-18 13:01:32 -0500

    Template for vision processing class

---
+++ src/org/usfirst/frc178/custom/VisionProcessing.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc178.custom;
+
+import org.usfirst.frc178.components.*;
+
+public class VisionProcessing {
+
+	private Drivetrain drivetrain;
+	private Shooter shooter;
+
+	public void VisionProcessing(Drivetrain drivetrain, Shooter shooter) {
+		this.drivetrain = drivetrain;
+		this.shooter = shooter;
+	}
+
+}
