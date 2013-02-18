a6a6a8a8bf4de455da8d0ab139fa3d9f3da23f45
Brandon Bald <brandonbald13@gmail.com>
2013-02-18 15:56:15 -0500

    added import for and fixed OculusClient refrence in RobotTemplate, added comments describing what buttons do what in shooter.aux()

--- src/org/usfirst/frc178/components/Shooter.java
+++ src/org/usfirst/frc178/components/Shooter.java
@@ -8,55 +8,70 @@ import org.usfirst.frc178.*;
  */
 public class Shooter 
 {
-    private Pneumatics pneumatics;
+	private Pneumatics pneumatics;
 	private Motors motors;
-    private Sensors sensors;
-    private HumanControl humanControl;
+	private Sensors sensors;
+	private HumanControl humanControl;
 
-    public Shooter(Motors motors, Sensors sensors, HumanControl humanControl, Pneumatics pneumatics)
-    {
-        this.pneumatics = pneumatics;
+	public Shooter(Motors motors, Sensors sensors, HumanControl humanControl, Pneumatics pneumatics)
+	{
+		this.pneumatics = pneumatics;
 		this.motors = motors;
-        this.sensors = sensors;
-        this.humanControl = humanControl;
-    }
-    
-    public void run() {
-		this.aux();
-		
-        if(humanControl.joystickAux.getTrigger())
-        {
-            shooterStart();
-        }
-        else
-        {
-            shooterStop();
-        }
-    }
+		this.sensors = sensors;
+		this.humanControl = humanControl;
+	}
 	
+	public void run() {
+		this.aux();
+		if(humanControl.joystickAux.getTrigger()){
+			shooterStart();
+		}
+		else{
+			shooterStop();
+		}
+	}
+	/**
+	 * aux controller
+	 * receives button inputs
+	 */
 	public void aux()
 	{
-		humanControl.joystickAux.getRawButton(1);	//button A
-		humanControl.joystickAux.getRawButton(2);	//button B
-		humanControl.joystickAux.getRawButton(3);	//button X
-		humanControl.joystickAux.getRawButton(4);	//button Y
-		humanControl.joystickAux.getRawButton(5);	//button LeftBumper
-		humanControl.joystickAux.getRawButton(6);	//button RightBumper
-		humanControl.joystickAux.getRawButton(7);	//button Back
-		humanControl.joystickAux.getRawButton(8);	//button Start
-		humanControl.joystickAux.getRawButton(9);	//button LeftAnalogStickButton
-		humanControl.joystickAux.getRawButton(10);	//button RightAnalogStickButton
+		if(humanControl.joystickAux.getRawButton(1)){	//button A
+		//auto fire
+		}
+		if(humanControl.joystickAux.getRawButton(2)){	//button B
+		//breech
+		}
+		if(humanControl.joystickAux.getRawButton(3)){	//button X
+		//auto aim
+		}
+		if(humanControl.joystickAux.getRawButton(4)){	//button Y
+		//indexer
+		}
+		if(humanControl.joystickAux.getRawButton(5)){	//button LeftBumper
+		//extend hopper
+		}
+		if(humanControl.joystickAux.getRawButton(6)){	//button RightBumper
+		//autospin up
+		}
+		if(humanControl.joystickAux.getRawButton(7)){	//button Back
+		//auto kill
+		}
+		if(humanControl.joystickAux.getRawButton(8)){	//button Start
+		}
+		if(humanControl.joystickAux.getRawButton(9)){	//button LeftAnalogStickButton
+		}
+		if(humanControl.joystickAux.getRawButton(10)){	//button RightAnalogStickButton
+		}
 	}
-
-    public void shooterStart() {
+	 public void shooterStart() {
 		motors.shooterOne.set(1.0);
 		motors.shooterTwo.set(1.0);
-    }
-
-    public void shooterStop()
-    {
-        motors.shooterOne.set(0.0);
-        motors.shooterTwo.set(0.0);
-    }
+	}
+	 public void shooterStop()
+	{
+	 motors.shooterOne.set(0.0);
+	 motors.shooterTwo.set(0.0);
+	}
 	
 }
