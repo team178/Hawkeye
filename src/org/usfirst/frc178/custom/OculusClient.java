661f5074e999b92954916eaf425dc9f5b1343403
Brandon Cheng <bcheng.gt@gmail.com>
2013-02-17 23:46:40 -0500

    New methods: isConnected(), disconnect()

--- src/org/usfirst/frc178/custom/OculusClient.java
+++ src/org/usfirst/frc178/custom/OculusClient.java
@@ -19,9 +19,13 @@ public class OculusClient {
 	private InputStream is;
 	private OutputStream os;
 
+	private boolean isConnected;
+
 	public OculusClient(String ip, String port) {
 		this.ip = ip;
 		this.port = port;
+
+		this.isConnected = false;
 	}
 
 	public void connect() {
@@ -31,13 +35,31 @@ public class OculusClient {
 
 			this.is = sc.openInputStream();
 			this.os = sc.openOutputStream();
+
+			this.isConnected = true;
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
+	public void disconnect() {
+		this.is.close();
+		this.os.close();
+		this.sc.close();
+
+		this.isConnected = false;
+	}
+
+	public boolean isConnected() {
+		return this.isConnected;
+	}
+
 	public String request() {
+
+		if (this.isConnected()) {
+			this.connect();
+		}
 
 		String result = "no data";
 
@@ -46,8 +68,6 @@ public class OculusClient {
 			this.is.read(data);
 
 			result = new String(data);
-			//is.close(); os.close(); sc.close();
-
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
