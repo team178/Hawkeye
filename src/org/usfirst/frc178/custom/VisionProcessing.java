90961a22d09343514f4baa42a39bb3d941c29138
Brandon Cheng <bcheng.gt@gmail.com>
2013-03-23 11:16:57 -0400

    Connect after 5 seconds

--- src/org/usfirst/frc178/custom/VisionProcessing.java
+++ src/org/usfirst/frc178/custom/VisionProcessing.java
@@ -3,6 +3,8 @@ package org.usfirst.frc178.custom;
 import org.usfirst.frc178.components.*;
 import org.usfirst.frc178.devices.*;
 
+import edu.wpi.first.wpilibj.*;
+
 public class VisionProcessing implements Runnable {
 
 	private Drivetrain drivetrain;
@@ -10,15 +12,30 @@ public class VisionProcessing implements Runnable {
 	private HumanControl humanControl;
 	private OculusClient oculusClient;
 
+	private Timer timer;
+	private boolean connected;
+
 	public VisionProcessing(Drivetrain drivetrain, Shooter shooter, HumanControl humanControl, OculusClient oculusClient) {
 		this.drivetrain = drivetrain;
 		this.shooter = shooter;
 		this.humanControl = humanControl;
 		this.oculusClient = oculusClient;
+
+		this.timer = new Timer();
+		this.connected = false;
+
+		this.timer.start();
 	}
 
 	public void run() {
+
 		while(true) {
+			if (this.timer.get() < 5 && !this.connected) {
+				this.oculusClient.connect();
+				this.connected = true;
+				System.out.println("connecting");
+			}
+
 			if (humanControl.joystickMain.getTrigger()) { // X
 				this.turn();
 			}
