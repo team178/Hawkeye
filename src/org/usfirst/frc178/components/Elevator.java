2a3e20ad9bc699444774a7b59555bd39157eb13b
Brandon Cheng <bcheng.gt@gmail.com>
2013-04-12 18:06:44 -0400

    Elevator class (part of issue #7)

---
+++ src/org/usfirst/frc178/components/Elevator.java
@@ -0,0 +1,53 @@
+package org.usfirst.frc178.components;
+
+import org.usfirst.frc178.devices.Motors;
+import org.usfirst.frc178.devices.Sensors;
+
+import edu.wpi.first.wpilibj.PIDOutput;
+
+public class Elevator implements PIDOutput {
+	private Motors motors;
+	private Sensors sensors;
+
+	public Elevator(Motors motors, Sensors sensors) {
+		this.motors = motors;
+		this.sensors = sensors;
+	}
+
+	public void raise(double speed) {
+		if (speed < 0) {
+			throw new IllegalArgumentException();
+		}
+
+		if (!sensors.elevationLowSwitch.getState()) {
+			motors.elevator.set(speed);
+		} else {
+			stop();
+		}
+	}
+
+	public void lower(double speed) {
+		if (speed < 0) {
+			throw new IllegalArgumentException();
+		}
+
+		if (!sensors.elevationHighSwitch.getState()) {
+			motors.elevator.set(-speed);
+		} else {
+			stop();
+		}
+	}
+
+	public void stop() {
+		motors.elevator.set(0.0);
+	}
+
+	public void pidWrite(double output) {
+		if (output < 0) {
+			lower(-output);
+		} else {
+			raise(output);
+		}
+	}
+
+}
