aadc15deceb88f32cb9ffb48ee90a8bef70c218f
gluxon <bcheng.gt@gmail.com>
2013-04-12 19:28:16 -0400

    Remove EncoderMod. Not required after all.

--- src/org/usfirst/frc178/devices/Sensors.java
+++ src/org/usfirst/frc178/devices/Sensors.java
@@ -4,28 +4,29 @@ import com.sun.squawk.platform.posix.natives.LibC;
 import edu.wpi.first.wpilibj.*;
 import org.usfirst.frc178.custom.*;
 
-public class Sensors
-{
+public class Sensors {
+
 	public final short PHOTOCELL_THRESHOLD = 200;
-	public Encoder /*shooterOneEncoder, */shooterTwoEncoder, driveEncoderLeft, driveEncoderRight;
-	public EncoderMod shooterOneEncoder;
-	public LimitSwitch elevationLowSwitch, elevationHighSwitch, elevationLoadSwitch, pressureSwitch;
+
+	public Encoder shooterOneEncoder, shooterTwoEncoder, driveEncoderLeft, driveEncoderRight;
+	public LimitSwitch elevationLowSwitch, elevationHighSwitch, elevationLoadSwitch;
+	public LimitSwitch pressureSwitch;
 	public Photosensor shooterPhoto, feederPhoto;
 
-	public Sensors()
-	{
+	public Sensors() {
 			//driveEncoderLeft = new Encoder(1,2);			//DIO ports 1,2
 			//driveEncoderLeft = new Encoder(3,4);			//DIO ports 3,4
 			//shooterOneEncoder = new Encoder(5,6);			//DIO ports 5,6
 			shooterOneEncoder = new EncoderMod(5,6);
-			shooterOneEncoder.setDistancePerPulse(1/64d);
 			//shooterTwoEncoder = new Encoder(7,8);			//DIO ports 7,8
 			elevationHighSwitch = new LimitSwitch(9, false);
 			elevationLoadSwitch = new LimitSwitch(10, false);
 			elevationLowSwitch = new LimitSwitch(12, false);
-			pressureSwitch = new LimitSwitch(13,false);		//DIO port 14, cutoff at 120 psi --Check Normally Open State******
+			pressureSwitch = new LimitSwitch(13, false);		//DIO port 14, cutoff at 120 psi --Check Normally Open State******
 			shooterPhoto = new Photosensor(3);
 			//feederPhoto = new Photosensor(3);
+
+			shooterOneEncoder.setDistancePerPulse(1/64d);
 	}
 	
 	public void printEncoders(){
@@ -35,7 +36,13 @@ public class Sensors
 	public void printPhotocells(){
 		System.out.println(shooterPhoto.analog.getAverageValue());
 	}
-	
+
+	public void printLimitSwitches() {
+		System.out.print(elevationHighSwitch.getState() + "\t");
+		System.out.print(elevationLoadSwitch.getState() + "\t");
+		System.out.println(elevationLowSwitch.getState());
+	}
+
 	public void PID(){
 		
 	}
@@ -49,12 +56,6 @@ public class Sensors
 		double output = error * gain + setPoint;
 		double scaledOutput =(output/(setPoint * gain + setPoint));
 		return (scaledOutput * scaledOutput * scaledOutput);  //scaledOutput ^ 3
-	}
-
-	public void printLimitSwitches() {
-		System.out.print(elevationHighSwitch.getState() + "\t");
-		System.out.print(elevationLoadSwitch.getState() + "\t");
-		System.out.println(elevationLowSwitch.getState());
 	}
 
 }
